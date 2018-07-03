52853b9cf1715c0cbda6feb6eee0f0670893a70f
qingyun <qingyun@huangxiaokuan.local>
2018-07-03 09:14:57 +0800

    删除

--- thisis.java
+++ thisis.java
@@ -1,22 +1,6 @@
 public class thisis{
 	public static void main(String[] args) {
 
-		System.out.println("添加一句话");
-
-		System.out.println("分支");
-
-
-
-		System.out.println("二次换分支");
-
-		System.out.println("换分支");
-
-		
-		System.out.println("三次换分支");
-
-
-		System.out.println("四次换分支");
-		
 
 	}
 }
